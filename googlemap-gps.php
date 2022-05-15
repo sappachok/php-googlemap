91206a720222fb8cfffbf9b369e4786648ab3e14
sappachok <suppachok_sin@nstru.ac.th>
2022-05-15 13:44:27 +0700

    .

--- googlemap-gps.php
+++ googlemap-gps.php
@@ -31,9 +31,33 @@
             title: 'จุดเริ่มต้น'
         });
 
-		$.get('get_location_json.php', function(response) {
-			console.log(response);
-		});
+        var location = [
+            {
+                name: "จุดที่ 2",
+                lat: "8.460799",
+                lon: "99.859021"
+            },
+            {
+                name: "จุดที่ 3",
+                lat: "8.462062",
+                lon: "99.859686"
+            },
+            {
+                name: "จุดที่ 4",
+                lat: "8.462562",
+                lon: "99.859786"
+            }
+        ]
+
+        $.each(location, function(index, value) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(value.lat, value.lon),
+                map: map,
+                icon: 'new_marker.png',
+                title: value.name
+            });
+        });
+
     }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=API_Key&callback=initMap"></script>
