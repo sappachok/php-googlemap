288e0d440ae4edeca967ef25b97054e4679fa323
sappachok <suppachok_sin@nstru.ac.th>
2022-05-24 12:52:28 +0700

    .

---
+++ googlemap-gps-ajax-db.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> <!-- Responsive website -->
+    <title>GPS Tracker</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body bgcolor="gray">
+    <div id="googleMap" style="height:600px;" bgcolor="white">Show Google Map Here.</div>
+</body>
+<!-- Script view google map -->
+<script>
+    function initMap() {
+        var mapProp = {
+            center: new google.maps.LatLng(8.4636, 99.8620),
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            zoom: 17,
+        };
+
+        /* start show map */
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        /* show marker */
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(8.4636, 99.8620),
+            map: map,
+            icon: 'new_marker.png',
+            title: 'จุดเริ่มต้น'
+        });
+
+		$.get('get_location_db.php', function(response) {
+			console.log(response);
+		});
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=API_Key&callback=initMap"></script>
+
+</html>
