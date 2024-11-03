131043bb702f51eb1f67b45d022bc9a7dd3491a7
sappachok <suppachok_sin@nstru.ac.th>
2024-11-04 01:26:39 +0700

    add config

--- googlemap-simple.php
+++ googlemap-simple.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> <!-- Responsive website -->
     <title>GPS Tracker</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
+
+<?php
+    include("config.php");
+?>
 
 <body bgcolor="gray">
     <div id="googleMap" style="height:600px;" bgcolor="white">Show Google Map Here.</div>
@@ -23,6 +26,6 @@
         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=API_Key&callback=initMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=<?php echo $api_key; ?>&callback=initMap"></script>
 
 </html>
